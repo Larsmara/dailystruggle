dfb53919ffc00a7a91d0394b5cb7bd1e8e12ca38
Larsmara <lars.m.antonsen@gmail.com>
2022-02-27 18:17:52 +0100

    Added route handling for navbar

--- src/components/Layout/Layout.vue
+++ src/components/Layout/Layout.vue
@@ -12,51 +12,42 @@
                     </label>
                 </div>
                 <div class="flex-1">
-                    <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+                    <a class="btn btn-ghost normal-case text-xl">DailyStruggle</a>
                 </div>
                 <div class="flex-none hidden lg:block">
                     <ul class="menu menu-horizontal p-0">
-                        <li><a>Item 1</a></li>
-                        <li tabindex="0">
-                            <a>
-                                Parent
-                                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
-                                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                                </svg>
-                            </a>
-                            <ul class="p-2 bg-base-100">
-                                <li><a>Submenu 1</a></li>
-                                <li><a>Submenu 2</a></li>
-                            </ul>
+                        <li v-for="route in routes" :key="route.name" :tabindex="route.subroutes.length ? '0' : ''">
+                            <router-link v-if="!route.subroutes.length" :to="route.route">{{ route.name }}</router-link>
+                            <template v-else>
+                                <a>
+                                    {{ route.name }}
+                                    <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                                        <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                                    </svg>
+                                </a>
+                                <ul class="p-2 bg-base-100">
+                                    <li v-for="subRoute in route.subroutes" :key="subRoute.name">
+                                        <router-link :to="subRoute.route">{{ subRoute.name }}</router-link>
+                                    </li>
+                                </ul>
+                            </template>
                         </li>
-                        <li><a>Item 3</a></li>
                     </ul>
                 </div>
                 <div class="flex-none">
-                    <div class="dropdown dropdown-end">
-                        <label tabindex="0" class="btn btn-ghost btn-circle">
-                            <div class="indicator">
-                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path
-                                        stroke-linecap="round"
-                                        stroke-linejoin="round"
-                                        stroke-width="2"
-                                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-                                    />
-                                </svg>
-                                <span class="badge badge-sm indicator-item">8</span>
-                            </div>
-                        </label>
-                        <div tabindex="0" class="mt-3 card card-compact w-52 dropdown-content bg-base-100 shadow">
-                            <div class="card-body">
-                                <span class="font-bold text-lg">8 Items</span>
-                                <span class="text-info">Subtotal: $999</span>
-                                <div class="card-actions">
-                                    <button class="btn btn-primary btn-block">View cart</button>
-                                </div>
-                            </div>
+                    <button class="btn btn-ghost btn-circle">
+                        <div class="indicator">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path
+                                    stroke-linecap="round"
+                                    stroke-linejoin="round"
+                                    stroke-width="2"
+                                    d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+                                />
+                            </svg>
+                            <span class="badge badge-xs badge-primary indicator-item"></span>
                         </div>
-                    </div>
+                    </button>
                     <div class="dropdown dropdown-end">
                         <label tabindex="0" class="btn btn-ghost btn-circle avatar">
                             <div class="w-10 rounded-full">
@@ -94,6 +85,7 @@
 
 <script setup>
     import NavbarVue from './Navbar.vue';
+    import routes from '../../constants/routes';
 </script>
 
 <style scoped></style>
