d7648d45cc275e91559483f9ab2a42e62e77c93f
imsern <kristofferims@hotmail.com>
2022-03-01 15:26:59 +0100

    blank templates for profile, settings and expenses
    Added page files for profile, settings and expenses.

---
+++ src/modules/Budget/Expenses.vue
@@ -0,0 +1,9 @@
+<template>Expenses</template>
+
+<script setup></script>
+
+<style scoped></style>
+
+
+// LAGET DENNE FOR Å SJEKKE OM JEG HAR SKJØNT MAPPE STRUKTUREN RIKTIG
+// DETTE ER FOR EXPENSES PAGEN
